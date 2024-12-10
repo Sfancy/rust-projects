7ae277dee27fb168dc47d41636b02ba6713ea94e
YutaoWang <yutao.wang1993@gmail.com>
2024-12-10 23:02:18 +0800

    add project from home

--- error_handle/src/main.rs
+++ error_handle/src/main.rs
@@ -1,4 +1,5 @@
-use std::{arch::asm, fs::File, intrinsics::mir::Goto, io::ErrorKind};
+use std::fs::{self, File};
+use std::io::{self, ErrorKind, Read};
 
 #[allow(unused)]
 fn create_panic() {
@@ -30,7 +31,49 @@ fn open_unexist_file() {
 
 #[allow(unused)]
 fn use_unwrap() {
-    let greeting_file = File::open("hello.txt").unwrap();
+    // let greeting_file = File::open("hello.txt").unwrap();
+    let greeting_file =
+        File::open("hello.txt").expect("hello.txt should be included in this project");
+}
+
+#[allow(unused)]
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+#[allow(unused)]
+fn short_read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+#[allow(unused)]
+fn even_shorter_read_username_from_file() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+#[allow(unused)]
+fn ultimate_short_read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+#[allow(unused)]
+fn last_char_of_fist_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
 
 fn main() {
